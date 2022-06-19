aa68abd40c47424f9b120a40217e3166a02d5dab
Victoria Rouser <vic86rous@gmail.com>
2022-06-19 00:16:39 -0500

    added component for input, created new method

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,10 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //Added file scanner to generate words
+        //Added file scanner to generate words from the separate file
         Scanner scanner = new Scanner(new File("C:/Users/vic86/OneDrive/Documents/hangman-words.txt"));
+        //Added input scanner for players input
+        Scanner scanner1 = new Scanner(System.in);
 
         List<String> words = new ArrayList<>();
 
@@ -21,6 +23,24 @@ public class Main {
         //Added random function to get a word from the list
         Random rand = new Random();
         String word = words.get(rand.nextInt(words.size()));
-        System.out.print(word);
+        System.out.println(word);
+
+        List<Character> guess = new ArrayList<>();
+
+        //Created a method to keep track of the player guesses
+        printWordProgress(word, guess);
+
+        //Creating Loop to get the input from the player
+    }
+
+    private static void printWordProgress(String word, List<Character> guess) {
+        for (int i = 0; i < word.length(); i++) {
+            if (guess.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            } else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
     }
 }
