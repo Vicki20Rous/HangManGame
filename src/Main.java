28e1b55f722481a2560f2d6a57734f590a7646c4
Victoria Rouser <vic86rous@gmail.com>
2022-06-18 23:25:34 -0500

    add scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("\"C:\\Users\\vic86\\OneDrive\\Documents\\hangman-words.txt\""));
+
+        List<String> words = new ArrayList<>();
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+        }
     }
 }
